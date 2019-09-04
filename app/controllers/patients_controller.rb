f33b6528db3d3c79ad3a940923036fe9720e6f1f
taisaoko <ttd3calvin@gmail.com>
2019-09-04 19:32:51 -0400

    fix /patients

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -18,7 +18,7 @@ class PatientsController < ApplicationController
 
   post '/patients' do
     if logged_in?
-      if params[:name] == "" || params[:medical_record_number] == "" || params[date_of_birth] == ""
+      if params[:name] == "" || params[:medical_record_number] == "" || params[:date_of_birth] == ""
         redirect to "/patients/new"
       else
         @patient = current_user.patients.build(name: params[:name], medical_record_number: params[:medical_record_number], date_of_birth: params[:date_of_birth])
@@ -42,16 +42,40 @@ class PatientsController < ApplicationController
     end
   end
 
-  # GET: /patients/5/edit
-  get "/patients/:id/edit" do
-    erb :"/patients/edit.html"
+  get '/patients/:id/edit' do  
+    if logged_in?
+      @patient = Patient.find_by_id(params[:id])
+      if @patient && @patient.user == current_user
+        erb :'patients/edit'
+      else
+        redirect to '/patients'
+      end
+    else
+      redirect to '/login'
+    end
+  end
+ 
+  patch '/articles/:id' do #edit action
+  @article = Article.find_by_id(params[:id])
+  @article.title = params[:title]
+  @article.content = params[:content]
+  @article.save
+  redirect to "/articles/#{@article.id}"
   end
 
-  # PATCH: /patients/5
-  patch "/patients/:id" do
-    redirect "/patients/:id"
+  patch '/patients/:id' do
+    @patient = Patient.find_by_id(params[:id])
+    @patient.update(params[:patient])
+    if !params[:title][:name].empty?
+      @patient.titles << Title.create(params[:title])
+    end
+    if !params[:landmark][:name].empty?
+      @patient.landmarks << Landmark.create(params[:landmark])
+    end
+    @patient.save
+    redirect "/patients/#{@patient.id}"
   end
-
+  
   # DELETE: /patients/5/delete
   delete "/patients/:id/delete" do
     redirect "/patients"
