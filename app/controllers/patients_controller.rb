e42ddf904a262b5e865f0a994b99fe0be34635e8
taisaoko <ttd3calvin@gmail.com>
2020-04-08 17:24:10 -0400

    edit routes to update and delete patients

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -6,79 +6,75 @@ class PatientsController < ApplicationController
 
   # get patients/new to render a form to create new entry
   get '/patients/new' do
-    if logged_in?
-      redirect_if_not_logged_in
-      erb :'patients/new'
-    end
+    redirect_if_not_logged_in
+    erb :'patients/new'
   end
 
   # post patients to create a patient
   post '/patients' do
-    binding.pry
+    # binding.pry
     redirect_if_not_logged_in
-    # I want to create a new journal entry and save it to the DB
-    # I also only want to create a journal entry if a user is logged in
-    # I only want to save the entry if it has some content
+    # I want to create a new patient and save it to the DB 
+    # I also only want to create a patient if a nurse is logged in
+    # I only want to save the patient with some info
     if params[:name] != "" || params[:medical_record_number] != "" || params[:date_of_birth] != ""
       # create a new patient
       @patient = Patient.create(name: params[:name], nurse_id: current_user.id, medical_record_number: params[:medical_record_number], date_of_birth: params[:date_of_birth])
-      flash[:message] = "Patient successfully created." if @patient.id
+      # flash[:message] = "Patient successfully created." if @patient.id
       redirect "/patients/#{@patient.id}"
     else
-      flash[:errors] = "Something went wrong - you must provide content for your entry."
+      # flash[:errors] = "Something went wrong - you must provide content for your entry."
       redirect '/patients/new'
     end
-    # if logged_in?
-    #   if params[:name] == "" || params[:medical_record_number] == "" || params[:date_of_birth] == ""
-    #     redirect to "/patients/new"
-    #   else
-    #     @patient = current_user.patients.build(name: params[:name], medical_record_number: params[:medical_record_number], date_of_birth: params[:date_of_birth])
-    #     if @patient.save
-    #       redirect to "/patients/#{@patient.id}"
-    #     else
-    #       redirect to "/patients/new"
-    #     end
-    #   end
-    # else
-    #   redirect to '/login'
-    # end
   end
 
   get '/patients/:id' do
-    if logged_in?
-      @patient = Patient.find_by_id(params[:id])
-      erb :'patients/show'
-    else
-      redirect to '/login'
-    end
+    set_patient
+    erb :'patients/show'
   end
 
+  # This route should send us to patients/edit.erb, which will render an edit form
   get '/patients/:id/edit' do  
-    if logged_in?
-      @patient = Patient.find_by_id(params[:id])
-      if @patient && @patient.nurse == current_user
-        erb :'patients/edit'
-      else
-        redirect to '/patients'
-      end
+    redirect_if_not_logged_in
+    set_patient
+    if authorized_to_edit?(@patient)
+      erb :'/patients/edit'
     else
-      redirect to '/login'
+      redirect "nurses/#{current_user.id}"
     end
   end
  
   patch '/patients/:id' do
-    @patient = Patient.find_by_id(params[:id])
-    @patient.name = params[:name]
-    @patient.medical_record_number = params[:medical_record_number]
-    @patient.date_of_birth = params[:date_of_birth]
-    @patient.save
-    redirect "/patients/#{@patient.id}"
+    redirect_if_not_logged_in
+    # 1. find the journal entry
+    set_patient
+    if authorized_to_edit?(@patient) && params[:name] != ""
+      # 2. modify (update) the patient
+      @patient.update(params)
+      # 3. redirect to show page
+      redirect "/patients/#{@patient.id}"
+    else
+      redirect "nurses/#{current_user.id}"
+    end
+    # @patient.medical_record_number = params[:medical_record_number]
+    # @patient.date_of_birth = params[:date_of_birth]
+    # @patient.save
   end
   
-  delete '/patients/:id/delete' do 
-    @patient = Patient.find_by_id(params[:id])
-    @patient.delete
-    redirect to '/patients'
+  delete '/patients/:id' do 
+    set_patient
+    if authorized_to_edit?(@patient)
+      @patient.destroy
+      flash[:message] = "Successfully deleted that entry."
+      redirect to '/patients'
+    else
+      flash[:message] = "Successfully deleted that entry."
+    end
   end
   
+  private
+
+  def set_patient
+    @patient = Patient.find(params[:id])
+  end
 end
