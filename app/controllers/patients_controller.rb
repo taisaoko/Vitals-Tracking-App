f013a6fa08e0d5e9d25e8bbadebf1932dd00528f
taisaoko <ttd3calvin@gmail.com>
2020-04-08 16:37:51 -0400

    edit get and post /patients

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,36 +1,47 @@
 class PatientsController < ApplicationController
   get '/patients' do
-    if logged_in?
-      @patients= Patient.all
+    @patients= Patient.all
       erb :'patients/index'
-    else
-      redirect to '/login'
-    end
   end
 
+  # get patients/new to render a form to create new entry
   get '/patients/new' do
     if logged_in?
+      redirect_if_not_logged_in
       erb :'patients/new'
-    else
-      redirect to '/login'
     end
   end
 
+  # post patients to create a patient
   post '/patients' do
-    if logged_in?
-      if params[:name] == "" || params[:medical_record_number] == "" || params[:date_of_birth] == ""
-        redirect to "/patients/new"
-      else
-        @patient = current_user.patients.build(name: params[:name], medical_record_number: params[:medical_record_number], date_of_birth: params[:date_of_birth])
-        if @patient.save
-          redirect to "/patients/#{@patient.id}"
-        else
-          redirect to "/patients/new"
-        end
-      end
+    binding.pry
+    redirect_if_not_logged_in
+    # I want to create a new journal entry and save it to the DB
+    # I also only want to create a journal entry if a user is logged in
+    # I only want to save the entry if it has some content
+    if params[:name] != "" || params[:medical_record_number] != "" || params[:date_of_birth] != ""
+      # create a new patient
+      @patient = Patient.create(name: params[:name], nurse_id: current_user.id, medical_record_number: params[:medical_record_number], date_of_birth: params[:date_of_birth])
+      flash[:message] = "Patient successfully created." if @patient.id
+      redirect "/patients/#{@patient.id}"
     else
-      redirect to '/login'
+      flash[:errors] = "Something went wrong - you must provide content for your entry."
+      redirect '/patients/new'
     end
+    # if logged_in?
+    #   if params[:name] == "" || params[:medical_record_number] == "" || params[:date_of_birth] == ""
+    #     redirect to "/patients/new"
+    #   else
+    #     @patient = current_user.patients.build(name: params[:name], medical_record_number: params[:medical_record_number], date_of_birth: params[:date_of_birth])
+    #     if @patient.save
+    #       redirect to "/patients/#{@patient.id}"
+    #     else
+    #       redirect to "/patients/new"
+    #     end
+    #   end
+    # else
+    #   redirect to '/login'
+    # end
   end
 
   get '/patients/:id' do
