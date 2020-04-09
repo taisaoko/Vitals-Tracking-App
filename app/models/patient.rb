42a7bfe08b6af5e17c482c5bf7a95bd911778862
taisaoko <ttd3calvin@gmail.com>
2020-04-09 10:18:38 -0400

    add valid_params? method

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,8 @@
 class Patient < ActiveRecord::Base
     has_many :vitals
     belongs_to :nurse
+
+    def self.valid_params?(params)
+       return !params[:name].empty? && !params[:medical_record_number].empty? && !params[:date_of_birth].empty?
+    end
 end
