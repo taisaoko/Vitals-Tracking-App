a3be1975683faa2ae915942fabfbc20cd93a25ab
taisaoko <ttd3calvin@gmail.com>
2020-04-09 10:14:05 -0400

    edit vital model

--- app/models/vital.rb
+++ app/models/vital.rb
@@ -1,3 +1,7 @@
 class Vital < ActiveRecord::Base
     belongs_to :patient
+
+    def self.valid_params?(params)
+        return !params[:blood].empty? && !params[:blood_pressure].empty? && !params[:pulse].empty? && !params[:temperature].empty? && !params[:oxygen_level].empty?
+    end
 end
