9a3f593abde40a61fa4aa4e144d01aea1bd58630
taisaoko <ttd3calvin@gmail.com>
2020-04-09 14:53:48 -0400

    define formatted created at method

--- app/models/vital.rb
+++ app/models/vital.rb
@@ -4,4 +4,8 @@ class Vital < ActiveRecord::Base
     def self.valid_params?(params)
         return !params[:blood].empty? && !params[:blood_pressure].empty? && !params[:pulse].empty? && !params[:temperature].empty? && !params[:oxygen_level].empty?
     end
+
+    def formatted_created_at
+        self.created_at.strftime("%A, %d %b %Y %l:%M %p")
+    end
 end
