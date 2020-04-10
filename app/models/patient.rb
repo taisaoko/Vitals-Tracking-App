87ea36d4e5711d18d115a8c2df4141d291ff751b
taisaoko <ttd3calvin@gmail.com>
2020-04-10 09:01:48 -0400

    add has many-through AR association

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,6 +1,6 @@
 class Patient < ActiveRecord::Base
-    has_many :vitals
     belongs_to :nurse
+    has_many :vitals
 
     def self.valid_params?(params)
        return !params[:name].empty? && !params[:medical_record_number].empty? && !params[:date_of_birth].empty?
