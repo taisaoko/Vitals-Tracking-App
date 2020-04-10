c90ddf05ade605f98daaf5c1fb1b2bf98dada38a
taisaoko <ttd3calvin@gmail.com>
2020-04-10 09:01:33 -0400

    add has many-through AR association

--- app/models/nurse.rb
+++ app/models/nurse.rb
@@ -1,14 +1,15 @@
 class Nurse < ActiveRecord::Base
     has_many :patients
+    has_many :vitals, through: :patients
 
-    has_secure_password
+    has_secure_password # gives us access to the #authenticate method!
 
     def slug
         name.downcase.gsub(" ","-")
     end
     
     def self.find_by_slug(slug)
-        Nurse.all.find{|nurse| nurse.slug == slug}
+        all.find{|nurse| nurse.slug == slug}
     end
-
+   
 end
