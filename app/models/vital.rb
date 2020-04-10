98a1e9116eaa6961d4f1903e6f891c408af214ab
taisaoko <ttd3calvin@gmail.com>
2020-04-10 09:40:59 -0400

    edit AR association

--- app/models/vital.rb
+++ app/models/vital.rb
@@ -1,9 +1,11 @@
 class Vital < ActiveRecord::Base
-    belongs_to :patient
-       
-    def self.valid_params?(params)
-        return !params[:blood].empty? && !params[:blood_pressure].empty? && !params[:pulse].empty? && !params[:temperature].empty? && !params[:oxygen_level].empty?
-    end
+    has_many :patient_vitals
+    has_many :patients, through: :patient_vitals
+    has_many :nurses, through: :patients
+
+    # def self.valid_params?(params)
+    #     return !params[:blood].empty? && !params[:blood_pressure].empty? && !params[:pulse].empty? && !params[:temperature].empty? && !params[:oxygen_level].empty?
+    # end
 
     def formatted_created_at
         self.created_at.strftime("%A, %d %b %Y %l:%M %p")
