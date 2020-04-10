437390bce7192489c57f5d8c5a9f4f938541b17f
taisaoko <ttd3calvin@gmail.com>
2020-04-10 09:02:16 -0400

    add has many-through AR association

--- app/models/vital.rb
+++ app/models/vital.rb
@@ -1,6 +1,6 @@
 class Vital < ActiveRecord::Base
     belongs_to :patient
-    
+       
     def self.valid_params?(params)
         return !params[:blood].empty? && !params[:blood_pressure].empty? && !params[:pulse].empty? && !params[:temperature].empty? && !params[:oxygen_level].empty?
     end
