a5693cbc7d6f62a7c40e24b08c810ae642df0f61
taisaoko <ttd3calvin@gmail.com>
2020-04-10 09:41:11 -0400

    edit AR association

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,8 +1,10 @@
 class Patient < ActiveRecord::Base
     belongs_to :nurse
     has_many :vitals
+    has_many :patient_vitals
+    has_many :vitals, through: :patients_vitals
 
-    def self.valid_params?(params)
-       return !params[:name].empty? && !params[:medical_record_number].empty? && !params[:date_of_birth].empty?
-    end
+    # def self.valid_params?(params)
+    #    return !params[:name].empty? && !params[:medical_record_number].empty? && !params[:date_of_birth].empty?
+    # end
 end
