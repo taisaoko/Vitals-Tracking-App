96a53afe961ec9ee4c159e047b315b0263fcde4a
taisaoko <ttd3calvin@gmail.com>
2020-04-11 16:27:22 -0400

    delete slug method

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -4,13 +4,13 @@ class Patient < ActiveRecord::Base
     has_many :patient_vitals
     has_many :vitals, through: :patient_vitals
 
-    def slug
-        name.downcase.gsub(" ","-")
-    end
+    # def slug
+    #     name.downcase.gsub(" ","-")
+    # end
     
-    def self.find_by_slug(slug)
-        all.find{|patient| patient.slug == slug}
-    end
+    # def self.find_by_slug(slug)
+    #     all.find{|patient| patient.slug == slug}
+    # end
 
     # def self.valid_params?(params)
     #    return !params[:name].empty? && !params[:medical_record_number].empty? && !params[:date_of_birth].empty?
